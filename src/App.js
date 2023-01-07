926a52ca87fa470b261f833beca3b2833eab76af
Sazzad171 <sajjad.cse.diu@gmail.com>
2023-01-07 11:27:54 +0600

    update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import  "./assets/css/index.css"
 function App() {
   return (
     <div className="App">
-      <h1 className="underline">heading</h1>
+      <h1 className="underline font-extrabold">heading</h1>
     </div>
   );
 }
