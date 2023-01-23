88e14c94491ac82b142a372feab6f32233e1ac0a
Sazzad171 <sajjad.cse.diu@gmail.com>
2023-01-23 09:49:44 +0600

    update

--- src/pages/home/IndustriesSection.jsx
+++ src/pages/home/IndustriesSection.jsx
@@ -2,6 +2,15 @@ import React from 'react';
 
 // img import
 import icon1 from '../../assets/img/home/industries/icon-2.png';
+import icon2 from '../../assets/img/home/industries/icon-3.png';
+import icon3 from '../../assets/img/home/industries/icon-5.png';
+import icon4 from '../../assets/img/home/industries/icon-6.png';
+import icon5 from '../../assets/img/home/industries/icon-7.png';
+import icon6 from '../../assets/img/home/industries/icon-8.png';
+import icon7 from '../../assets/img/home/industries/icon-9.png';
+import icon9 from '../../assets/img/home/industries/icon-10.png';
+import icon10 from '../../assets/img/home/industries/icon-11.png';
+import icon11 from '../../assets/img/home/industries/icon-12.png';
 
 export default function IndustriesSection() {
   return (
@@ -17,6 +26,60 @@ export default function IndustriesSection() {
           </div>
           <div>
             <div className="flex flex-wrap -mx-2">
+              <div className="w-1/4 px-2 mb-4">
+                <div className='w-full h-30 bg-white rounded-tl-2xl rounded-br-2xl rounded-tr-[36px] rounded-bl-[36px] text-center p-4'>
+                  <img src={ icon1 } alt="icon" className="max-w-full h-auto mx-auto mb-2" />
+                  <p className="font-blue">Banking Industries</p>
+                </div>
+              </div>
+              <div className="w-1/4 px-2 mb-4">
+                <div className='w-full h-30 bg-white rounded-tl-2xl rounded-br-2xl rounded-tr-[36px] rounded-bl-[36px] text-center p-4'>
+                  <img src={ icon1 } alt="icon" className="max-w-full h-auto mx-auto mb-2" />
+                  <p className="font-blue">Banking Industries</p>
+                </div>
+              </div>
+              <div className="w-1/4 px-2 mb-4">
+                <div className='w-full h-30 bg-white rounded-tl-2xl rounded-br-2xl rounded-tr-[36px] rounded-bl-[36px] text-center p-4'>
+                  <img src={ icon1 } alt="icon" className="max-w-full h-auto mx-auto mb-2" />
+                  <p className="font-blue">Banking Industries</p>
+                </div>
+              </div>
+              <div className="w-1/4 px-2 mb-4">
+                <div className='w-full h-30 bg-white rounded-tl-2xl rounded-br-2xl rounded-tr-[36px] rounded-bl-[36px] text-center p-4'>
+                  <img src={ icon1 } alt="icon" className="max-w-full h-auto mx-auto mb-2" />
+                  <p className="font-blue">Banking Industries</p>
+                </div>
+              </div>
+              <div className="w-1/4 px-2 mb-4">
+                <div className='w-full h-30 bg-white rounded-tl-2xl rounded-br-2xl rounded-tr-[36px] rounded-bl-[36px] text-center p-4'>
+                  <img src={ icon1 } alt="icon" className="max-w-full h-auto mx-auto mb-2" />
+                  <p className="font-blue">Banking Industries</p>
+                </div>
+              </div>
+              <div className="w-1/4 px-2 mb-4">
+                <div className='w-full h-30 bg-white rounded-tl-2xl rounded-br-2xl rounded-tr-[36px] rounded-bl-[36px] text-center p-4'>
+                  <img src={ icon1 } alt="icon" className="max-w-full h-auto mx-auto mb-2" />
+                  <p className="font-blue">Banking Industries</p>
+                </div>
+              </div>
+              <div className="w-1/4 px-2 mb-4">
+                <div className='w-full h-30 bg-white rounded-tl-2xl rounded-br-2xl rounded-tr-[36px] rounded-bl-[36px] text-center p-4'>
+                  <img src={ icon1 } alt="icon" className="max-w-full h-auto mx-auto mb-2" />
+                  <p className="font-blue">Banking Industries</p>
+                </div>
+              </div>
+              <div className="w-1/4 px-2 mb-4">
+                <div className='w-full h-30 bg-white rounded-tl-2xl rounded-br-2xl rounded-tr-[36px] rounded-bl-[36px] text-center p-4'>
+                  <img src={ icon1 } alt="icon" className="max-w-full h-auto mx-auto mb-2" />
+                  <p className="font-blue">Banking Industries</p>
+                </div>
+              </div>
+              <div className="w-1/4 px-2 mb-4">
+                <div className='w-full h-30 bg-white rounded-tl-2xl rounded-br-2xl rounded-tr-[36px] rounded-bl-[36px] text-center p-4'>
+                  <img src={ icon1 } alt="icon" className="max-w-full h-auto mx-auto mb-2" />
+                  <p className="font-blue">Banking Industries</p>
+                </div>
+              </div>
               <div className="w-1/4 px-2 mb-4">
                 <div className='w-full h-30 bg-white rounded-tl-2xl rounded-br-2xl rounded-tr-[36px] rounded-bl-[36px] text-center p-4'>
                   <img src={ icon1 } alt="icon" className="max-w-full h-auto mx-auto mb-2" />
