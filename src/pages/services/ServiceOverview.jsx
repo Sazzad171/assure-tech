657ef68315827313e8226314cbae3b5d4f7de1d6
Sazzad171 <sajjad.cse.diu@gmail.com>
2023-02-27 09:49:42 +0600

    update

---
+++ src/pages/services/ServiceOverview.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ServiceOverview() {
+  return (
+    <section className="pt-28 pb-20">
+      
+    </section>
+  )
+}
---
+++ src/pages/services/TechnologyUsed.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function TechnologyUsed() {
+  return (
+    <section className="pt-28 pb-20">
+      
+    </section>
+  )
+}
