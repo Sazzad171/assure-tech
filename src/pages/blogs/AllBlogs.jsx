063922e42cf37b18f2f0d81ea3875ba45f664e19
Sazzad171 <sajjad.cse.diu@gmail.com>
2023-03-08 13:06:12 +0600

    update

--- src/pages/blogs/AllBlogs.jsx
+++ src/pages/blogs/AllBlogs.jsx
@@ -15,12 +15,17 @@ export default function AllBlogs() {
               </a>
             </div>
 
-            <div>
-
+            <div className='grid grid-cols-2 gap-2 bg-[linear-gradient(to_right,#ff0048_0,#c300c3_100%)] rounded-3xl mx-4 -mt-4 px-4 py-2 z-10 relative'>
+              <div>
+                <p className='text-sm text-white'>Web Application</p>
+              </div>
+              <div>
+                <p className='text-right text-sm text-white'>May 1 2023</p>
+              </div>
             </div>
 
-            <div className='px-6 py-7'>
-              <h4 className='text-blue font-semibold text-2xl font-poppins mb-4'>
+            <div className='px-6 pt-5 pb-7'>
+              <h4 className='text-blue font-semibold text-2xl font-poppins mb-3'>
                 <a href="#f">Best technology for Web Development</a>
               </h4>
               <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
